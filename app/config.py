f4da46cc3f4f3839d2a9cca212343ab29db643c5
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2023-05-14 19:04:50 -0400

    Avoid to upload important credentials

--- app/config.py
+++ app/config.py
@@ -2,9 +2,6 @@ from app.password import Password
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
 class Config:
     SECRET_KEY = Password().generate_password(10)
     DEBUG = True
